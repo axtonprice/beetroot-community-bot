0b7918c71917429c349e0b35d4d4ffd52b68a119
Axton <enquires@axtonprice.com>
2022-03-21 01:06:24 +0000

    Update core.js

--- commands/drugstore/core.js
+++ commands/drugstore/core.js
@@ -27,7 +27,7 @@ module.exports = {
                 Core Functions
             */
 
-            function mainDisplayPage() {
+            function generalDisplay() {
                 // Global Stats Variables
                 var highestBalance = 0;
                 var highestBalanceUser = "Artisan_#4387";
@@ -38,7 +38,6 @@ module.exports = {
                 var authorStoreBalance = 0;
                 var authorStoreName = `${authorUserName}'s Store`;
                 var authorTotalSoldItems = 0;
-
                 const embed = new Discord.MessageEmbed()
                     .setTitle('Beetroot Drugstore :pill:')
                     .setAuthor(message.author.tag, message.guild.iconURL())
@@ -49,11 +48,10 @@ module.exports = {
                         { name: 'Global Statistics', value: `Top User: \`$${highestBalance} - ${highestBalanceUser}\`\nTotal Stores: \`${totalStoreCount} ($${totalStoresBalance})\``, inline: true },
                         { name: 'Manage Your Store', value: `\`\`\`${prefix}store work » Begin working to earn cash\n${prefix}store delete » Delete your store\`\`\``, inline: false },
                         { name: 'Beetroot Economy', value: `\`\`\`${prefix}store buy » Buy an item from a users store\n${prefix}store browse » View list of popular stores\`\`\``, inline: false },
-                        // \n${prefix}store buy <store-id> <item-id> » Buy an item from a store
                     );
                 message.reply({ embeds: [embed] });
             }
-            function createShop() {
+            function myStoreCreate() {
                 var authorUserId = message.author.id;
                 var authorUserName = message.author.username;
                 var jsonData = JSON.stringify("{\n \"components\": {\n \"store_details\": {\n \"store_name\": \"Axtons Meth Lab\",\n \"store_description\": \"Come get yo ass meth n shit bruv dickhead\",\n \"last_purchase\": \"2022-03-19 03:42:21.000000\",\n \"store_balance\": \"0\"\n },\n \"store_menu\": {\n \"001\": {\n \"drug_name\": \"Weed\",\n \"drug_description\": \"Devils lettuce blud\",\n \"date_added\": \"2022-03-19 03:42:21.000000\"\n }\n },\n \"store_customers\": {\n \"441994490115391488\": {\n \"item_id_purchase\": \"001\",\n \"sales_price\": \"100\",\n \"purchase_date\": \"2022-03-19 03:42:21.000000\"\n }\n }\n }\n}\n");
@@ -64,7 +62,7 @@ module.exports = {
                     .setDescription(`Successfully created \`${authorUserName}'s Drug Store\`! \nUse \`${prefix}drugstore\` to view drugstore commands!`);
                 message.reply({ embeds: [embed] });
             }
-            function deleteShop() {
+            function myStoreDelete() {
                 var authorUserId = message.author.id;
                 var authorUserName = message.author.username;
                 if (args[1] === "confirm") {
@@ -82,17 +80,31 @@ module.exports = {
                     message.reply({ embeds: [embed] });
                 }
             }
+            function economyStartWorking() {
+                var authorUserId = message.author.id;
+                var authorUserName = message.author.username;
+                var randomNumber = Math.floor(Math.random() * 10);
+
+                connection.query("DELETE FROM `drug_stores` WHERE `store_owner_id` = '" + authorUserId + "'", (error, results, fields) => { });
+                const embed = new Discord.MessageEmbed()
+                    .setTitle('Beetroot Drugstore :pill:')
+                    .setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }))
+                    .setDescription(`Successfully deleted \`${authorUserName}'s Drug Store\`! \nUse \`${prefix}drugstore\` to view drugstore commands!`);
+                message.reply({ embeds: [embed] });
+            }
 
             /* 
                 Function Loader Handler 
             */
-           
+
             if (args[0] === "create") {
-                createShop(); // Create a new drugstore
+                myStoreCreate(); // Create a new drugstore
             } else if (args[0] === "delete") {
-                deleteShop(); // Delete users drugstore
+                myStoreDelete(); // Delete users drugstore
+            } else if (args[0] === "work") {
+                economyStartWorking(); // Delete users drugstore
             } else {
-                mainDisplayPage(); // User has a store, display main page
+                generalDisplay(); // User has a store, display main page
             }
 
             connection.end();
